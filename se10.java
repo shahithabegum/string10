14fc8b15752c7fb3de9c22e385c6bd94e631f8d3
shahithabegum <69446892+shahithabegum@users.noreply.github.com>
2021-11-11 15:45:46 +0530

    Add files via upload

---
+++ se10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+class se10
+{
+  public static void main(String arg[])
+   {
+     try
+         {
+            String s="ilovemycountry";
+              System.out.println(s.length());
+	}
+	catch(Exception e)
+	{
+	  System.out.println(e);
+	}
+       }
+}
